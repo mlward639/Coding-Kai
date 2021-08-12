de9c87ed641b7e67c0843c27519c71ad676fed1c
Maggie Ward <mlward639@gmail.com>
2021-08-11 21:40:27 -0400

    added addl files/folders to structures, added basic server with test route to index.html

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const path = require('path');
+// ADD IF NEEDED: const helpers = require('./utils/helpers');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// IS any of this needed with mongoose (rather than mysql)
+// const sequelize = require("./config/connection");
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// const sess = {
+//   secret: "Super secret secret",
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   logging: false,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
+
+// app.use(session(sess));
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// app.use(require('./index.html'));
+//app.use(express.static(path.join(__dirname, 'public'))); // are we using a public folder or client folder?? update accordingly. for now, just connecting to test index.html
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// routes
+// app.use(require('./controllers'));
+//test route
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
