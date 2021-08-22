081974af814ff9eca2f86e4b11666d92828a1ee5
Maggie Ward <mlward639@gmail.com>
2021-08-22 02:48:59 -0400

    story page with styling-need final touches on styling/colors. still need to update btn functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Player from "./components/player";
 import Homepage from "./pages/Homepage";
+import Storypage from "./pages/Storypage";
 
+//routing NOT working
 export default function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/story">
+            <Storypage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
