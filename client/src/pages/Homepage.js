d22d467cc0ae2824347a12442e0b99a4a1da34e3
Maggie Ward <mlward639@gmail.com>
2021-08-22 01:47:08 -0400

    homepage with cards component and styling. still need to update with routes/mongo and add functionality to buttons

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -4,14 +4,22 @@ import React from "react";
 // import { QUERY_PROFILES } from "../utils/queries";
 import CharacterCards from "../components/characterCards/CharacterCards";
 
+const username = "Test User"; //update with get request data from mongo db
+
 const Homepage = () => {
   // const { loading, data } = useQuery(QUERY_PROFILES);
   // const profiles = data?.profiles || [];
 
   return (
-    <div className="homePage">
-      <CharacterCards />
-    </div>
+    <>
+      <h1 className="welcomeHomepage">Welcome, {username}!</h1>
+      <div className="characterCardsContainer">
+        <CharacterCards />
+      </div>
+      <div className="startNewGameContainer">
+        <button className="startNewGame">Start a new game</button>
+      </div>
+    </>
   );
 };
 
