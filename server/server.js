1e08854d174bdf99a20adf6c987ce18b0cb3db3a
Zack <zacharythekhan@gmail.com>
2021-08-25 15:11:11 -0400

    seeding

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
 
-
+module.exports = mongoose.connection; // required for seeds
 
 
 //====================================================================================================
