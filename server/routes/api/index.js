1d2488d511fcde0449872cfea27ee40c45778aa3
csbonang <csgbonang@gmail.com>
2021-08-26 20:44:03 -0700

    seed path are up to date, seeds data work with model, and questionRoutes is changed to .find

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,16 +1,10 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-=======
 // const gameRoutes = require('./gameroutes');
->>>>>>> efe463b248ed0c6e356297be883834c9593d6ea4
 const characterRoutes = require('./characterRoutes');
 const enemyRoutes = require('./enemyRoutes');
 const questionRoutes = require('./questionRoutes');
 
-<<<<<<< HEAD
-=======
 // router.use('/', gameRoutes);
->>>>>>> efe463b248ed0c6e356297be883834c9593d6ea4
 router.use('/character', characterRoutes);
 router.use('/enemy', enemyRoutes);
 router.use('/question', questionRoutes);
