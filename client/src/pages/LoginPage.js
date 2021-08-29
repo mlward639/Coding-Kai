ad7b72b46f75125b92434ff2546aa199029ed515
Roomsiejones <seangravito@gmail.com>
2021-08-29 16:10:02 -0500

    uncommented the stuff I commented out to let me test the stuff I needed to

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,23 +1,23 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import Auth from "../utils/auth";
-// import LoginCard from "../components/authentication/loginCard";
-// import Footer from "../components/authentication/footer";
-// import Header from "../components/authentication/header";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
+import LoginCard from "../components/authentication/loginCard";
+import Footer from "../components/authentication/footer";
+import Header from "../components/authentication/header";
 
-// const LoginPage = () => {
-//   return (
-//     <>
-//       <div className="loginPageContainer">
-//         <Header />
-//         <LoginCard />
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// };
+const LoginPage = () => {
+  return (
+    <>
+      <div className="loginPageContainer">
+        <Header />
+        <LoginCard />
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-// export default LoginPage;
+export default LoginPage;
 
 // DO WE WANT A SELECT CHARACTER PAGE (so when they click start new game, it takes them to select character page)?? Or just randomly assign them a character??
 // I think they should get m2.png a their character. 
